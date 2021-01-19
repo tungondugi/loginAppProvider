241b36b1ccf13a63559f41adcfc57764efffff0a
tungon dugi <tungondugi@gmail.com>
2021-01-20 04:00:47 +0530

    Created a custom MaterialSwatch color

--- loginAppProvider/lib/main.dart
+++ loginAppProvider/lib/main.dart
@@ -6,13 +6,33 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  // Creating a custom swatch
+  final MaterialColor customTealColor = MaterialColor(
+    0xff00BFA6,
+    const <int, Color>{
+      50: Color(0xff00BFA6),
+      100: Color(0xff00BFA6),
+      200: Color(0xff00BFA6),
+      300: Color(0xff00BFA6),
+      400: Color(0xff00BFA6),
+      500: Color(0xff00BFA6),
+      600: Color(0xff00BFA6),
+      700: Color(0xff00BFA6),
+      800: Color(0xff00BFA6),
+      900: Color(0xff00BFA6),
+    },
+  );
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: customTealColor,
+        brightness: Brightness.light,
         visualDensity: VisualDensity.adaptivePlatformDensity,
+        scaffoldBackgroundColor: Colors.white,
       ),
       home: Registration(),
     );
