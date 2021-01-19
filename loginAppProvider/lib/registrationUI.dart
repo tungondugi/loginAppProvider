e9ed2a5387e411a124e827563634d013b97b975c
tungon dugi <tungondugi@gmail.com>
2021-01-20 04:01:56 +0530

    Created welcome UI

--- loginAppProvider/lib/registrationUI.dart
+++ loginAppProvider/lib/registrationUI.dart
@@ -1,29 +1,83 @@
 import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
 
 class Registration extends StatelessWidget {
   Widget build(BuildContext context) {
+    // This will hide the status bar and the bottom bar
+    // SystemChrome.setEnabledSystemUIOverlays([]);
+
     return Scaffold(
-      appBar: AppBar(),
-      body: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            SizedBox(
-              child: Image.asset(
-                "assets/image/welcome.png",
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 2,
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/image/welcome.png"),
+                  fit: BoxFit.contain,
+                ),
               ),
             ),
-            Text(""),
-            ElevatedButton(
-              child: Text("Log In"),
-              onPressed: () {},
+          ),
+          Text(
+            "Welcome",
+            style: TextStyle(
+              fontSize: 20,
             ),
-            ElevatedButton(
-              child: Text("Sign Up"),
-              onPressed: () {},
+          ),
+          Expanded(
+            flex: 1,
+            child: Padding(
+              padding: EdgeInsets.all(16),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  SizedBox(
+                    height: 40,
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        elevation: 0,
+                        onPrimary: Colors.white,
+                      ),
+                      child: Text(
+                        "Log In",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                  SizedBox(
+                    height: 25,
+                  ),
+                  SizedBox(
+                    height: 40,
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          primary: Colors.white,
+                          elevation: 0,
+                          onPrimary: Colors.teal,
+                          side: BorderSide(
+                            color: Colors.teal[300],
+                          )),
+                      child: Text(
+                        "Sign Up",
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
