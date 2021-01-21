e4c02c6885f8cd96661ab31696d0a5be37d578b5
tungon dugi <tungondugi@gmail.com>
2021-01-21 23:06:44 +0530

    Written a model class for the signUP form.

--- loginAppProvider/lib/validation/signUpValidationModel.dart
+++ loginAppProvider/lib/validation/signUpValidationModel.dart
@@ -1,1 +1,13 @@
+/// [Ther Model Class]
+class ValidationItem {
+  // This class is the Model for the signUp validation.
+  // NOTE: Everything that comes out of a TextField is a string.
+  final String value;
+  // the value will store the valid data that will be used to validate the validation items.
+  final String error;
+  // and if any error(does not satisfies the validation criteria) occurs then this
+  // error message will pop up.
 
+  // A constructor for mapping the variables to the class.
+  ValidationItem(this.value, this.error);
+}
